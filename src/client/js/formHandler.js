a9a1a8db6dffb047a95b53900e7d47159ba3e614
dreyes <dreyesb5@gmail.com>
2020-05-24 13:30:45 -0600

    request to Aylien running through express server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,12 +5,29 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    console.log("::: Form Submitted :::");
+    postData('https://twitter.com/nathanbarry/status/1264195055968940033')
+    console.log("::: Form Sent :::")
+
 }
+
+const postData = async (mysite = '') => {
+    const response = await fetch('http://localhost:8080/analyze', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ mysite: mysite }),
+    })
+    try {
+        document.getElementById('results').innerHTML = mysite;
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch(error) {
+        console.log("error", error);
+    }
+};
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
 var AYLIENTextAPI = require('aylien_textapi');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 var textapi = new AYLIENTextAPI({
     application_id: process.env.API_ID,
@@ -12,13 +14,15 @@ var textapi = new AYLIENTextAPI({
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -26,13 +30,14 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    textapi.sentiment({
-        'text': 'John is a very good football player!'
+app.post('/analyze', (req, res) => {
+  console.log(req.body.mysite)
+  textapi.sentiment({
+        url: req.body.mysite
       }, function(error, response) {
         if (error === null) {
           console.log(response);
+          res.send(response);
         }
       });
-    res.send(mockAPIResponse)
-})
+});
